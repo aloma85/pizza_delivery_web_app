fb036f21b20ae896edf4bdef9a8e777d9a6e8de0
aloma85 <idrisun22@gmail.com>
2021-03-19 21:12:20 -0500

    Push 09:12

---
+++ migrations/versions/378921957e32_.py
@@ -0,0 +1,16 @@
+"""empty message
+
+Revision ID: 378921957e32
+Revises:
+Create Date: 2021-03-19 21:06:46.466594
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '378921957e32'
+down_revision = None
+branch_labels = None
+depends_on = None
