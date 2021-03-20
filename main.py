66033d186ee5035c04f754ba43a73bac44f19c41
aloma85 <idrisun22@gmail.com>
2021-03-19 20:23:36 -0500

    Push 08:23

--- main.py
+++ main.py
@@ -8,15 +8,32 @@ from flask_admin import Admin
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField, SelectField, IntegerField, DecimalField, SelectMultipleField
 
-
 basdir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 login_manager = LoginManager()
 
 
-
-
-
 app.config["SECRET_KEY"]="Youwillneverguess"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basdir , "pizza.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db=SQLAlchemy(app)
+Migrate(app,db)
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+admin = Admin(app)
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(userid)
+
+
+
+
+class User(db.Model , UserMixin):
+    id = db.Column(db.Integer , primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
