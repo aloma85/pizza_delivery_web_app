5138d9cc7cc170a49c70253f16a0e522ed6f40cc
aloma85 <idrisun22@gmail.com>
2021-03-19 19:45:01 -0500

    Push 7:44

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from flask import Flask, render_template,request , redirect , flash , url_for , 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from flask_login import LoginManager,UserMixin,login_required,current_user,login_user,logout_user
+
+
+basdir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+login_manager = LoginManager()
+
+
+
+
+
+app.config["SECRET_KEY"]="Youwillneverguess"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basdir , "pizza.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
